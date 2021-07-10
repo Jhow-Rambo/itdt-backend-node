71e1458496935e016c461994fc0fd6f970b065af
Jhonata L. Rambo <jhonatan.ramboo@hotmail.com>
2021-07-10 16:22:50 -0300

    feat: create entities for inferences

---
+++ src/modules/inference/typeorm/entities/Inference.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('inferences')
+class Inference {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  normal_image: string;
+
+  @Column()
+  inferred_image: string;
+
+  @Column()
+  inference: JSON;
+
+  @Column()
+  created_at: string;
+}
+
+export default Inference;
